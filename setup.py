683d7cfc0b2f5bfcc6253d881c87307368f617ed
maelrktsn <61709481+maelrktsn@users.noreply.github.com>
2025-09-03 00:25:09 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mrlib',
+    version='1.1.5',
+    description='my personal library',
+    author='mael.rktsn@gmail.com',
+    packages=find_packages(),
+    include_package_data=True
+)
